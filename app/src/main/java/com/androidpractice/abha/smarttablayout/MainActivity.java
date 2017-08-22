032f91bd7b345de937271cf7a4084cb85007c14a
abha_genecorp <abha.c06@gmail.com>
2017-08-22 12:48:26 +0530

    Smart tab layout library included

--- app/src/main/java/com/androidpractice/abha/smarttablayout/MainActivity.java
+++ app/src/main/java/com/androidpractice/abha/smarttablayout/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private SectionsPagerAdapter sectionsPagerAdapter;
-    private TabLayout tabLayout;
+//    private TabLayout tabLayout;
+    private SmartTabLayout smartTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(){
         viewPager = (ViewPager) findViewById(R.id.container);
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        smartTabLayout = (SmartTabLayout) findViewById(R.id.tabs);
 
         sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), 3);
         sectionsPagerAdapter.addFragment(new FragmentOne(), "Frag One");
@@ -29,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
         sectionsPagerAdapter.addFragment(new FragmentThree(), "Frag Three");
 
         viewPager.setAdapter(sectionsPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+//        tabLayout.setupWithViewPager(viewPager);
+        smartTabLayout.setViewPager(viewPager);
 
 
     }
